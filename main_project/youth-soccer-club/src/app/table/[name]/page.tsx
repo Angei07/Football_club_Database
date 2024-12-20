ec53cafaae04776252ab12e0339c71aceac78c7b
Angei07 <angelgakiza@gmail.com>
2024-12-20 09:39:03 -1200

    Create page.tsx

---
+++ main_project/youth-soccer-club/src/app/table/[name]/page.tsx
@@ -0,0 +1,31 @@
+import Table from "@/app/_components/Table";
+import { getDb } from "@/app/_util/db";
+import { RowDataPacket } from "mysql2";
+
+interface Row extends RowDataPacket {}
+
+async function getRows(tableName: string) {
+  const db = await getDb();
+  const [rows] = await db.execute<Row[]>(
+    `SELECT * FROM ${tableName} LIMIT 100`
+  );
+  return rows;
+}
+
+export default async function TablePage({
+  params,
+}: {
+  params: { name: string };
+}) {
+  const rows = await getRows(params.name).catch(() => null);
+
+  if (!rows) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Table tableName={params.name} rows={rows} editable />
+    </div>
+  );
+}
