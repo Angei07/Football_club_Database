387956b5a91065daea376f1d07ef9f1c408b2d36
Angei07 <angelgakiza@gmail.com>
2024-12-20 10:02:48 -1200

    Create query8.sql

---
+++ main_project/youth-soccer-club/sql/queries/query8.sql
@@ -0,0 +1,26 @@
+SELECT
+    sfm.FirstName AS SecondaryFirstName,
+    sfm.LastName AS SecondaryLastName,
+    sfm.TelephoneNumber AS SecondaryPhoneNumber,
+    cm.ClubMembershipNumber,
+    cm.FirstName AS ClubMemberFirstName,
+    cm.LastName AS ClubMemberLastName,
+    cm.DateOfBirth,
+    cm.SocialSecurityNumber,
+    cm.MedicalCardNumber,
+    cm.TelephoneNumber AS ClubMemberPhoneNumber,
+    cm.Address,
+    cm.City,
+    cm.Province,
+    cm.PostalCode,
+    sfr.Relationship
+FROM
+    FamilyMember fm
+JOIN
+    SecondaryFamilyMember sfm ON fm.FamilyMemberID = sfm.FamilyMemberID
+JOIN
+    SecondaryFamilyRelated sfr ON sfm.SecondaryFamilyMemberID = sfr.SecondaryFamilyMemberID
+JOIN
+    ClubMembers cm ON sfr.ClubMemberID = cm.ClubMemberID
+WHERE
+    fm.FamilyMemberID = ? /* 1 OR 2 */;
